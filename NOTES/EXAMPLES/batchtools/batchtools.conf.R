c9bb117e10fb30e67a7643a2ea876c0380610bd2
myoung3 <michaeltyoung@gmail.com>
2019-06-19 12:41:45 -0700

    Update batchtools.conf.R

--- NOTES/EXAMPLES/batchtools/batchtools.conf.R
+++ NOTES/EXAMPLES/batchtools/batchtools.conf.R
@@ -1,8 +1,8 @@
 
-default.resources = list(queue="mesa.q", group="mesa",ncpus=2,blas.threads=2)
+default.resources = list(queue="mesa.q", group="mesa",ncpus=1,blas.threads=1)
  
-#by default, give a SGE task 2 cores and tells the math library mkl to use 2 threads.
- #this effectively makes it so each task gets a full dedicated physical core and doesn't share that core with other SGE tasks. setting blas.threads allows the possiblity that  R may make use of hyperthreading via mkl (depends on the R function)
+#by default, give a SGE task 1 cores and tells the math library mkl to use 1 threads.
+#depending on your application, it may be better to switch this to ncpus=2, blas.threads=2. 
  #note that you can also set max.concurrent.jobs here. this might be a way to prevent any individual process from ever using a certain number of cores, regardless of how many are available.
 
 
